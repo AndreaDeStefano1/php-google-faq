aaf85856d93373e3cec3076f53ff4abc5649d1ce
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-06-08 15:37:26 +0200

    Stampo le domande e risposte in pagina

--- index.php
+++ index.php
@@ -49,3 +49,32 @@ $db = [
   ],
 ]
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <header>
+    <div class="header-top">
+      logo
+    </div>
+    <div class="header-bot">
+      link
+    </div>
+  </header>
+  <main>
+    <?php 
+    foreach ($db as $faq) {
+     foreach ($faq as $key => $value) {
+       echo '<br> '. $key . '<br> ' .  $value;
+     }
+    }
+    ?>
+  </main>
+</body>
+</html>
